c1f56dc9a178d813871ea61978d8561f07998d67
Joe1Jun <junker91joe@gmail.com>
2024-12-13 17:46:24 +0000

    Initialised the printwriter and bufferedreader the oth print a question to the client and assign the value returned to an instance variable

--- src/ie/atu/sw/Server.java
+++ src/ie/atu/sw/Server.java
@@ -2,6 +2,7 @@ package ie.atu.sw;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -34,6 +35,8 @@ public class Server implements Runnable{
 		// To print information our
 		private PrintWriter out;
 		
+		private String nickname;
+		
 		public ConnectionHandler(Socket client) {
 			this.client = client;
 			
@@ -42,6 +45,25 @@ public class Server implements Runnable{
 		@Override
 		public void run() {
 			
+			try {
+				// This is the output stream initialised and the auto flush set to through so there is 
+				// no need to manually flush the output stream
+				out = new PrintWriter(client.getOutputStream(), true);
+				// This will read in the input stream from the client
+				in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				
+				
+				// ****Add edge cases etc to make this more robust****
+				out.println("Please enter a nickname");
+				// assigns the variable the value of the line read from the client
+				nickname = in.readLine();
+				
+				
+			} catch (IOException e) {
+				// TODO: handle exception
+				
+				
+			}
 			
 		}
 		
