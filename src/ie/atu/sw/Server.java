73e2326872e06ac37885426c8440792b961003d0
Joe1Jun <junker91joe@gmail.com>
2024-12-13 17:40:04 +0000

    Added basic functionality to the server side set up to accept the client connection

--- src/ie/atu/sw/Server.java
+++ src/ie/atu/sw/Server.java
@@ -1,15 +1,21 @@
 package ie.atu.sw;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server implements Runnable{
 
+	private int PORT = 9999;
 	@Override
 	public void run() {
 		try {
-			ServerSocket server = new ServerSocket(9999);
-			
+			// This creates a server socket bound to a specific port
+			ServerSocket server = new ServerSocket(PORT);
+			// This waits for the client connection and accepts it 
+			Socket client = server.accept();
 			
 		} catch (IOException e) {
 			// Handle this later
@@ -21,10 +27,21 @@ public class Server implements Runnable{
 	
 	
 	class ConnectionHandler implements Runnable {
+	    // This will hold the value of the current instance of the Socket class;
+		private Socket client;
+		//To read information in 
+		private BufferedReader in;
+		// To print information our
+		private PrintWriter out;
+		
+		public ConnectionHandler(Socket client) {
+			this.client = client;
+			
+		}
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
+			
 			
 		}
 		
