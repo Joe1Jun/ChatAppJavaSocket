88ef439a64e765d033be032b19bc754d716f3ca4
Joe1Jun <junker91joe@gmail.com>
2024-12-19 15:21:20 +0000

    Added and initiaised the buffered reader and print writer for sending and receiving data via the server

--- src/ie/atu/sw/Client.java
+++ src/ie/atu/sw/Client.java
@@ -1,21 +1,30 @@
 package ie.atu.sw;
 
-import java.io.IOException;
 import java.net.Socket;
+import java.io.*;
 
 public class Client implements Runnable{
 	// Port number on which the server is running
-	public static final int DAYTIME_PORT = 9999;
+	public static final int PORT = 9999;
 	// Hostname of the server to connect to, set to localhost for testing on the same machine
-	private String hostname = "localhost"; 
+	private Socket client;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	
 	@Override
 	public void run() {
 		
 		try {
 			// Create a new socket and connect it to the specified hostname and port
             // This establishes a TCP connection between the client and the server
-			Socket socket = new Socket(hostname, DAYTIME_PORT);
-			
+			client = new Socket("127.0.0.1", PORT);
+			// Initialises the output stream for sending data to the server
+			// The 'true' parameter enables auto-flushing, so there is no need to call flush manually after each println
+			out = new PrintWriter(client.getOutputStream(), true);
+			// Initialises the input stream for receiving data from the server
+			// Wraps the socket's input stream with InputStreamReader and BufferedReader for efficient reading of character streams
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			
 			
 		} catch (IOException e) {
